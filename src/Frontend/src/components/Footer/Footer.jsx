fb5ca69e0a92530cac30ecf727e30185ca9646ff
Penguin <ishanjaiswal370@gmail.com>
2024-08-11 01:10:10 +0530

    added navbar footer components

--- src/Frontend/src/components/Footer/Footer.jsx
+++ src/Frontend/src/components/Footer/Footer.jsx
@@ -1,5 +1,24 @@
 const Footer = () => {
-    return <>Footer</>;
+    return (
+        <div className="footer">
+            Made with ❤️ by{" "}
+            <a
+                href="https://www.github.com/IJPenguin"
+                className="footer_link
+            "
+            >
+                Ishan
+            </a>{" "}
+            and{" "}
+            <a
+                href="https://www.github.com/ImCYMBIOT"
+                className="footer_link
+            "
+            >
+                Agnivesh
+            </a>
+        </div>
+    );
 };
 
 export default Footer;
--- src/Frontend/src/main.jsx
+++ src/Frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./routes/App";
+import "./main.scss";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
